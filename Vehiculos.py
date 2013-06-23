bc377e4132f330ca872e9805ad82e354eb6130da
Baeza <baeza@baeza-Inspiron-5421.(none)>
2013-06-23 12:26:59 -0400

    Segunda Version de la Interfaz

--- Vehiculos.py
+++ Vehiculos.py
@@ -9,7 +9,48 @@ class Main (QtGui.QMainWindow):
 		super(Main, self).__init__()
 		self.ui =  Ui_MainWindow()
 		self.ui.setupUi(self)
+		self.set_signals()
+		self.set_listeners()
 		self.show()
+	def set_signals(self):
+		self.ui.btn_nuevo_ve.clicked.connect(self.nuevo_ve)
+		self.ui.btn_editar_ve.clicked.connect(self.editar_ve)
+		self.ui.btn_eliminar_ve.clicked.connect(self.eliminar_ve)
+
+		self.ui.btn_nuevo_ma.clicked.connect(self.nuevo_ma)
+		self.ui.btn_editar_ma.clicked.connect(self.editar_ma)
+		self.ui.btn_eliminar_ma.clicked.connect(self.eliminar_ma)
+	def nuevo_ve(self):
+		print "Nuevo Vehiculo"
+	def editar_ve(self):
+		print "Editar Vehiculo"
+	def eliminar_ve(self):
+		print "Eliminar Vehiculo"
+	
+	def nuevo_ma(self):
+		print "Nueva Marca"
+	def editar_ma(self):
+		print "Editar Marca"
+	def eliminar_ma(self):
+		print "Eliminar Marca"
+
+	def set_listeners(self):
+		self.ui.tab.currentChanged[int].connect(self.tab_cambio)
+		self.ui.le_filtro_ve.textChanged[str].connect(self.cargar_vehiculo_por_busqueda)
+		self.ui.le_filtro_ma.textChanged[str].connect(self.cargar_marca_por_busqueda)
+	def cargar_vehiculo_por_busqueda(self):
+		print "Buscar vehiculos"
+	def cargar_marca_por_busqueda(self):
+		print "Buscar marcas"
+	def tab_cambio(self):
+		if self.ui.tab.currentIndex()==0:
+			self.ui.le_filtro_ve.setText("")
+			self.ui.le_filtro_ma.setText("")
+			print "tabla vehiculos"
+		if self.ui.tab.currentIndex()==1:
+			self.ui.le_filtro_ve.setText("")
+			self.ui.le_filtro_ma.setText("")
+			print "tabla marca"
 
 def run():
 	app = QtGui.QApplication(sys.argv)
